f27bcf4a74331cfdf17a0d3901dc4b2925dc4694
yarbala <yarbala@gmail.com>
2023-06-26 09:56:49 +0200

    build multiplatform - decrease number of platforms

--- php82/build_and_push_multiplatform.sh
+++ php82/build_and_push_multiplatform.sh
@@ -3,5 +3,9 @@
 # Exit on any error
 set -e
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v8 -t yarbala/php8-fpm-laravel-nginx:8.2 --push -f nginx.Dockerfile .
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v8 -t yarbala/php8-laravel-cli:8.2 --push -f cli.Dockerfile .
+#docker buildx create --use --name buildx_instance
+
+docker buildx build --platform linux/amd64,linux/arm64 -t yarbala/php8-fpm-laravel-nginx:8.2 --push -f nginx.Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 -t yarbala/php8-laravel-cli:8.2 --push -f cli.Dockerfile .
+
+#docker buildx stop buildx_instance
